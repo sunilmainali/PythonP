bb4821318f47711a143153ce665ab37590793b1f
sunil <sunilmainali5881@gmail.com>
2024-08-23 11:05:54 +0545

    variables and data types

---
+++ l2_varaiables.py
@@ -0,0 +1,35 @@
+#1. Write a python program to add two numbers
+# a= int(input('enter a number a:'))
+# b= int(input('enter a number b:'))
+# c=a+b
+# print(f"the sum of a and b is {c}")
+
+ #Write a python program to find remainder 
+ # when a number is divided by z
+
+# a=15
+# b=4
+# c=a%b
+# print(c)
+
+#3 . Check the type of variable assigned using input () function.
+# a= input('enter a number a:')
+# print(type(a))
+
+#4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+#‘b’ or not. Take a = 34 and b = 80
+# a = 34  
+# b = 80
+# c=a>b
+# print(c)
+
+#5. Write a python program to find an average of two numbers entered by the user.
+# a= int(input('enter a number a:'))
+# b= int(input('enter a number b:'))
+# c=(a+b)/2
+# print(f"the average of a and b is {c}")
+
+#6. Write a python program to calculate the square of a number entered by the user.
+a= float(input('enter a number a:'))
+c=a*a
+print(f"the square of a is {c}")
