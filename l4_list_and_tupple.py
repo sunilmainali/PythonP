680b1e6649e45a9f40ecf67bacc920e0ae484fd1
sunil <sunilmainali5881@gmail.com>
2024-08-25 08:37:15 +0545

    list and tupple

---
+++ l4_list_and_tupple.py
@@ -0,0 +1,50 @@
+#1. Write a program to store seven fruits in a list entered by the user.
+
+# fruits=[]
+# f1=input("enter fruit name: ")
+# fruits.append(f1)
+# f2=input("enter fruit name: ")
+# fruits.append(f2)
+# f3=input("enter fruit name: ")
+# fruits.append(f3)
+# f4=input("enter fruit name: ")
+# fruits.append(f4)
+# f5=input("enter fruit name: ")
+# fruits.append(f5)
+# f6=input("enter fruit name: ")
+# fruits.append(f6)
+# f7=input("enter fruit name: ")
+# fruits.append(f7)
+# print(fruits)
+
+#2. Write a program to accept marks of 6 students and display them in a sorted
+# marks=[]
+# f1=int(input("enter marks : "))
+# marks.append(f1)
+# f2=int(input("enter marks : "))
+# marks.append(f2)
+# f3=int(input("enter marks : "))
+# marks.append(f3)
+# f4=int(input("enter marks : "))
+# marks.append(f4)
+# f5=int(input("enter marks : "))
+# marks.append(f5)
+# f6=int(input("enter marks : "))
+# marks.append(f6)
+
+# marks.sort()
+# print(marks)
+
+# 3. Check that a tuple type cannot be changed in python.
+# t=(1,2,3,4)
+# t[0]=5
+# print(t)
+
+#4. Write a program to sum a list with 4 numbers.
+# l=[1,2,3,4]
+# print(sum(l))
+
+#5. Write a program to count the number of zeros in the following tuple:
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
+
